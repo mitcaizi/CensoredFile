0e58eb23dbd25aedb721a2e0902519533a106449
mitcaizi <jling1@mail.csuchico.edu>
2018-03-29 22:00:15 -0700

    File

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author:Jingwen Ling
 #include <iostream>
 #include <fstream>
 
@@ -6,17 +6,26 @@ using namespace std;
 
 int main()
 {
-  ifstream in("example.txt");
-  char first = ' ', last = ' ', current;
+        ifstream in("example.txt");
+        char first = ' ', last = ' ', current;
 
-  //Read from the file opened by in, and the first letter (either upper or
-  //lower case) should be assigned to first and the last letter (again in
-  //either case) from the file to the variable last
+        //Read from the file opened by in, and the first letter (either upper or
+        //lower case) should be assigned to first and the last letter (again in
+        //either case) from the file to the variable last
 
+        while (!in.eof())
+        {
+                in >> current;
+                if (current >='a' && current <= 'z' || current >= 'A' && current <= 'Z')
+                                {
+                                if (first == ' ')
+                                first = current;
+                                last = current;
+                                }
+                                }
 
+                                //Then, finally, this displays the censored version:
+                                cout<<first<<"**"<<last<<endl;
 
-  //Then, finally, this displays the censored version:
-  cout<<first<<"**"<<last<<endl;
-
-  return 0;
-}
+                                return 0;
+                                }
